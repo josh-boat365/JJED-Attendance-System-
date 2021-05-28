f73bd8c6ce0ca9766e66750f9961172b5ac1a29f
Intern <intern@turntabl.io>
2021-05-28 09:41:27 -0700

    update on main.py and jjed.py and jjed.db

---
+++ jjed_db.py
@@ -0,0 +1,74 @@
+import sqlite3
+
+connection = sqlite3.connect("jjed.db")
+cursor = connection.cursor()
+
+try:
+    create_intern_table = """CREATE TABLE IF NOT EXISTS interns(
+    intern_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    intern_name TEXT NOT NULL, 
+	intern_school TEXT NOT NULL,
+	intern_level TEXT NOT NULL,
+	intern_contact TEXT NOT NULL,
+	intern_email TEXT NOT NULL
+    )"""
+
+    create_admin_table = """CREATE TABLE IF NOT EXISTS admins(
+        admin_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        admin_name TEXT NOT NULL, 
+        admin_email TEXT NOT NULL,
+        admin_password TEXT NOT NULL
+    )"""
+
+    create_activity_table = """CREATE TABLE IF NOT EXISTS activities(
+        activity_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        activity_title TEXT NOT NULL,
+        activity_content TEXT NOT NULL,
+        activity_datetime DATETIME GETDATE
+    )"""
+
+    create_attendance_table = """CREATE TABLE IF NOT EXISTS attandance(
+        attendance_bool TEXT NOT NULL,
+        attendance_datetime DATETIME GETDATE,
+        intern_id INTEGER PRIMARY KEY,
+        FOREIGN KEY (intern_id)
+        REFERENCES interns (intern_id) 
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+    )"""
+    cursor.execute(create_intern_table)
+    connection.commit()
+    cursor.execute(create_admin_table)
+    connection.commit()
+    cursor.execute(create_activity_table)
+    connection.commit()
+    cursor.execute(create_attendance_table)
+    connection.commit()
+except connection.Error as error:
+    print("debug print",error)
+finally:
+    if connection:
+        connection.close()
+
+
+
+def insert_list_data(mlist):
+    connection = sqlite3.connect("jjed.db")
+    cursor = connection.cursor()
+    try:
+        sql = """ INSERT INTO interns(intern_name, intern_school,intern_level,intern_contact,intern_email)VALUES(?,?,?,?,?)"""
+        cursor.executemany(sql, mlist)
+        connection.commit()
+        print("commited")
+    except connection.Error as error:
+        print(error)
+    finally:
+        connection.close()
+intern = [("mery", "University of Cape Coast", "Level 300", "0558027244", "maryakua3@gmail.com"),
+         ("joshuA", "Accra Technical University", "Level 200", "0245553697", "joshua.goat19@gmail.com"),
+          ("sem", "University of Cape Coast", "Level 300", "0569775844", "enochsem@gmail.com"),
+          ("Eem", "University of Cape k", "Level 300", "0569765844", "jojosem@gmail.com"),
+          ("Jojo", "Ashesi University", "Level 300", "0558027244", "jojo@gmail.com")
+           ]
+
+insert_list_data(intern)
--- main.py
+++ main.py
@@ -1,13 +1,54 @@
 """main function """
 
 from flask import Flask, render_template, url_for,  request, redirect
+import sqlite3
+
+
 
 app = Flask(__name__)
 
 
 
-@app.route("/")
+def insert_list_data(mlist):
+    connection = sqlite3.connect("jjed.db")
+    cursor = connection.cursor()
+    try:
+        sql = """ INSERT INTO interns(intern_name, intern_school,intern_level,intern_contact,intern_email)VALUES(?,?,?,?,?)"""
+        cursor.executemany(sql, mlist)
+        connection.commit()
+        print("commited")
+    except connection.Error as error:
+        print(error)
+    finally:
+        connection.close()
+
+intern = [("Mary Dufie Afrane", "University of Cape Coast", "Level 300", "0558027244", "maryakua3@gmail.com"),
+         ("Joshua Nyarko Boateng", "Accra Technical University", "Level 200", "0245553697", "joshua.goat19@gmail.com"),
+          ("Enoch Sem", "University of Cape Coast", "Level 300", "0569775844", "enochsem@gmail.com"),
+          ("jojo Sem", "University of Cape k", "Level 300", "0569765844", "jojosem@gmail.com"),
+          ("Jojo ", "Ashesi University", "Level 300", "0558027244", "jojo@gmail.com")
+
+           ]
+# insert_list_data(intern)
+
+def update_db():
+    pass
+# form_data = request.form()
+# insert_list_data(form_data)
+
+@app.route("/", methods=["POST","GET"])
 def index():
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        connection = sqlite3.connect("jjed.db")
+        cursor = connection.cursor()
+        query = "SELECT intern_name FROM interns WHERE intern_name = {n}".format(n=name)
+        cursor.execute(query)
+        if cursor.fetchall():
+            return redirect(url_for('intern_home'))
+        else:
+            return redirect(url_for('index'))
     return render_template("intern_login.html")
 
 @app.route("/admin_home")
